bc45cc8434538f68870a801aa7c9bf65901e45f1
stackoverprof <mail.errbint@gmail.com>
2022-03-04 23:27:57 +0700

    feat: simple component

---
+++ components/SimpleComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+// import { useTailwind } from 'tailwind-rn';
+
+const SimpleComponent = () => {
+	// const tailwind = useTailwind();
+	return (
+		<View>
+			<View>
+				<Text>Hello Tailwind</Text>
+			</View>
+		</View>
+	);
+};
+
+export default SimpleComponent;
