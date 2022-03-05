4171de856ab23cafcc2b322fea22f99558333782
stackoverprof <mail.errbint@gmail.com>
2022-03-05 13:54:24 +0700

    feat: custom utility classes

--- core/tailwind/tailwind.config.js
+++ core/tailwind/tailwind.config.js
@@ -1,3 +1,6 @@
+// tailwind.config.js
+const { plugin } = require('twrnc');
+
 module.exports = {
 	content: ['./**/*.{html,js,jsx,ts,tsx}'],
 	theme: {
@@ -7,5 +10,39 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(({ addUtilities }) => {
+			addUtilities({
+				'btn-primary': `px-4 py-3 bg-green rounded`,
+
+				'flex-cc': `flex justify-center items-center`,
+				'flex-cs': `flex justify-center items-start`,
+				'flex-ce': `flex justify-center items-end`,
+
+				'flex-bc': `flex justify-between items-center`,
+				'flex-bs': `flex justify-between items-start`,
+				'flex-be': `flex justify-between items-end`,
+
+				'flex-sc': `flex justify-start items-center`,
+				'flex-ss': `flex justify-start items-start`,
+				'flex-se': `flex justify-start items-end`,
+
+				'flex-ec': `flex justify-end items-center`,
+				'flex-es': `flex justify-end items-start`,
+				'flex-ee': `flex justify-end items-end`,
+
+				'flex-vc': `flex justify-evenly items-center`,
+				'flex-vs': `flex justify-evenly items-start`,
+				'flex-ve': `flex justify-evenly items-end`,
+
+				'flex-ac': `flex justify-around items-center`,
+				'flex-as': `flex justify-around items-start`,
+				'flex-ae': `flex justify-around items-end`,
+
+				col: `flex-col`,
+
+				full: `w-full h-full`,
+			});
+		}),
+	],
 };
--- screens/AppScreen/TabOneScreen.tsx
+++ screens/AppScreen/TabOneScreen.tsx
@@ -1,10 +1,13 @@
-import { Text, View } from 'react-native';
+import { Text, View, Pressable } from 'react-native';
 import tw from '../../core/tailwind';
 
 const TabOneScreen = () => {
 	return (
-		<View style={tw`flex justify-center items-center w-full h-full bg-white`}>
+		<View style={tw`flex-cc w-full h-full bg-white`}>
 			<Text style={tw`mb-4 text-lg font-bold text-blue-300`}>Home</Text>
+			<Pressable style={tw`btn-primary`}>
+				<Text style={tw`font-bold text-white`}>Btn Primary</Text>
+			</Pressable>
 		</View>
 	);
 };
