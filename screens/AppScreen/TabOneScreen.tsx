2f98ce1a159fc794b298364cd5918719bd75bf88
stackoverprof <mail.errbint@gmail.com>
2022-03-05 22:52:24 +0700

    feat: update content

--- screens/AppScreen/TabOneScreen.tsx
+++ screens/AppScreen/TabOneScreen.tsx
@@ -4,7 +4,7 @@ import tw from '../../core/tailwind';
 const TabOneScreen = () => {
 	return (
 		<View style={tw`flex-cc bg-white full`}>
-			<Text style={tw`mb-4 text-lg font-bold text-blue-300`}>Home</Text>
+			<Text style={tw`mb-4 text-lg font-bold text-blue-300`}>Expo Starter</Text>
 			<Pressable style={tw`btn-primary`}>
 				<Text style={tw`font-bold text-white`}>Btn Primary</Text>
 			</Pressable>
--- screens/AuthScreen/index.tsx
+++ screens/AuthScreen/index.tsx
@@ -4,7 +4,7 @@ import tw from '../../core/tailwind';
 const AuthScreen = () => {
 	return (
 		<View style={tw`flex justify-center items-center w-full h-full bg-white`}>
-			<Text style={tw`mb-4 text-lg font-bold text-green`}>YOOOOO UPSELL POS!</Text>
+			<Text style={tw`mb-4 text-lg font-bold text-green`}>Auth Screen</Text>
 		</View>
 	);
 };
